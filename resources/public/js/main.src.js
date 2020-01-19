845ed2c4e9abe440fb1264a866cf877985289b90
A.Lepe <dev@alepe.com>
2020-01-19 23:04:30 +0900

    v1.0 : Update and Delete are now working.

--- resources/public/js/main.src.js
+++ resources/public/js/main.src.js
@@ -2,7 +2,7 @@ var apiUrl = "/api/v1";
 //var apiUrl = "http://rest-demo.alepe.com/api/v1";
 
 document.addEventListener("DOMContentLoaded", function() {
-
+    var editId = 0;
     /**
      * Return the path to the profile photo
      */
@@ -13,18 +13,29 @@ document.addEventListener("DOMContentLoaded", function() {
      * Update the profile view
      */
     function updateProfile(id) {
-        $get(apiUrl + "/person/" + id, function(person) {
-            photo.show = true;
+        if(id) {
+            $get(apiUrl + "/person/" + id, function(person) {
+                photo.show = true;
+                photo.img.src = getProfilePhoto(id);
+                profile.name.value = person.first_name + " " + person.last_name;
+                profile.age.value = person.age;
+                profile.colour.div.css = person.favourite_colour;
+                profile.colour.i = person.favourite_colour;
+                profile.hobbies.items.length = 0;
+                for(var h in person.hobby) {
+                    profile.hobbies.items.push(person.hobby[h]);
+                }
+                editId = person.id;
+            });
+        } else {
             photo.img.src = getProfilePhoto(id);
-            profile.name = person.first_name + " " + person.last_name;
-            profile.age = person.age;
-            profile.colour.div.css = person.favourite_colour;
-            profile.colour.i = person.favourite_colour;
+            profile.name.value = "";
+            profile.age.value = "";
+            profile.colour.div.css = "white";
+            profile.colour.i = "";
             profile.hobbies.items.length = 0;
-            for(var h in person.hobby) {
-                profile.hobbies.items.push(person.hobby[h]);
-            }
-        });
+            editId = 0;
+        }
     }
     /**
      * Wrapper for success
@@ -50,7 +61,18 @@ document.addEventListener("DOMContentLoaded", function() {
           position: "bottom"
         });
     }
+    /*
+     * Wrapper for confirmations
+     */
+    function yn(msg, yes, no) {
+        notie.confirm({
+          text: "Are you sure you want to " + msg + "?",
+          cancelText: 'No',
+          position: "top",
+        }, yes, no || function() {});
+    }
 
+    // Main table list. We are showing only the first 100
     var table = m2d2("#table", function(callback) {
         $get(apiUrl + "/people/0/100", function(row) {
             var people = [];
@@ -66,6 +88,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         var row = e.target;
                         var id = this.dataset.id;
                         updateProfile(id);
+                        return false;
                     },
                     photo : {
                         img : {
@@ -89,6 +112,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
+    // Photo in profile
     var photo = m2d2("#photo", {
         show : false,
         img : {
@@ -96,15 +120,50 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    // Profile settings
     var profile = m2d2("#profile", {
-        css : "",
-        name : "",
-        age  : "",
+        form : {
+            css : "",
+            onsubmit : function(e) {
+                var elems = this.elements;
+                var data = {
+                    first_name : elems.name.value.split(" ")[0],
+                    last_name : elems.name.value.split(" ")[1],
+                    age : elems.age.value
+                }
+                if(editId) {
+                    $put(apiUrl + "/person/"+editId, data, function(res) {
+                        ok("Saved successfully!");
+                        table.m2d2.update();
+                    }, function() {
+                        ng("Error while saving data.");
+                    });
+                }
+                return false;
+            }
+        },
+        name : {
+            value : "",
+            required : true,
+            readOnly : true,
+            placeholder: "First Last"
+        },
+        age  : {
+            value : "",
+            min : 1,
+            max : 110,
+            required : true,
+            readOnly : true
+        },
         colour : {
             div : {
                 css : ""
             },
             i : ""
+        },
+        hobby : {
+            value : "",
+            readOnly : true
         },
         hobbies : {
             template: "li",
@@ -112,41 +171,72 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    var actions = m2d2("#actions", {
+        rm : {
+            onclick : function() {
+                yn("delete", function() {
+                    if(editId) {
+                        $delete(apiUrl + "/person/"+editId, function(res) {
+                            updateProfile(0);
+                            table.m2d2.update();
+                        });
+                    }
+                });
+                return false;
+            }
+        }
+    });
+
+    // Login button
     var login = m2d2("#login", {
         show: true,
         onclick: function() {
-            notie.input({
+            var value = "admin";
+            /*notie.input({
               text: "Please input password",
               submitText: 'Login',
               position: "top",
               type: 'password'
-            }, function(value) {
+            }, function(value) {*/
                 $post(apiUrl + "/auth/login", {
                     user: "admin",
                     pass: value
                 }, function(){
                     login.show = false;
                     logout.show = true;
-                    profile.css = "edit";
+                    profile.form.css = "edit";
+                    // Set editable fields:
+                    profile.name.readOnly = false;
+                    profile.age.readOnly = false;
+                    profile.hobby.readOnly = false;
                     ok("Login succeed!");
                 }, function() {
                     ng("Login failed!");
                 });
-            }, function (value) {});
+           // }, function (value) {});
+            return false;
         }
     });
 
+    // Logout button
     var logout = m2d2("#logout", {
         show: false,
         onclick: function() {
             $get(apiUrl + "/auth/logout", function() {
                 login.show = true;
                 logout.show = false;
-                profile.css = "";
+                profile.form.css = "";
+                // Readonly fields:
+                profile.name.readOnly = true;
+                profile.age.readOnly = true;
+                profile.hobby.readOnly = true;
+                ok("Logged out");
             });
+            return false;
         }
     });
 
+    // Hobby list (all options)
     var hobby_list = m2d2("#hobby_list", {
         template : "option",
         items : hobbiesFullList
